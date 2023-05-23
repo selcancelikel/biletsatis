d2a85dc54fccee19e9adf6f3c021a70cd6574620
selcancelikel <95944187+selcancelikel@users.noreply.github.com>
2023-05-23 22:30:29 +0300

    Delete git directory

--- git/girisform.php
+++
@@ -1,9 +0,0 @@
-<?php
-include "config.php";
-?>
-
-<form action="giris.php" method="POST">
-Kullaniciadi: <input type="text" name="uname"><br>
-Şifre: <input type="password" name="upass"><br>
-<input type="submit" value="Giriş Yap!">
-</form>
